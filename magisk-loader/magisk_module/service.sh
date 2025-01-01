fec7ee6d269c2ba640cc9b5e3bf19440e205fb1b
yxy-os <134496652+yxy-os@users.noreply.github.com>
2025-01-01 17:50:36 +0800

    up

--- magisk-loader/magisk_module/service.sh
+++ magisk-loader/magisk_module/service.sh
@@ -4,9 +4,6 @@ until [ `getprop sys.boot_completed` -eq 1 ]; do
 	sleep 1
 done
 
-if [ -f /system/etc/aria2/aria2.conf ]; then
-    aria2c --conf-path=/system/etc/aria2/aria2.conf -D
-fi
-if [ -d /system/etc/lighttpd/www ]; then
-    lighttpd -f /system/etc/lighttpd/lighttpd.conf
-fi
+aria2c --conf-path=/system/etc/aria2/aria2.conf -D
+
+lighttpd -f /system/etc/lighttpd/lighttpd.conf
